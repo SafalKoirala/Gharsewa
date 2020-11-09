b4ba7bd75ed7da92f61778e0996a6e7a9b3735bb
SafalKoirala <safalkoirala92@gmail.com>
2020-11-09 21:35:35 +0545

    insert operation done partially

--- inc/dbconn.php
+++ inc/dbconn.php
@@ -3,10 +3,9 @@
     $db="gharsewa";
     $user="root";
     $pass="";
-   $conn= mysqli_connect($host,$user,$pass,$db);
-
-   if(!$conn){
-    die("Connection failed because ".mysqli_connect_error());
+    $dsn="mysql:host=$host;dbname=$db";
+    $pdo =new PDO($dsn,$user,$pass); 
       
-   }
+   
+   
   ?>
--- staff-register.php
+++ staff-register.php
@@ -1,57 +1,56 @@
-<?php include('inc\head.php')?>
-<?php include('inc\nav.php')?>
-<?php 
- include('inc\dbconn.php')?>
+<?php include('inc/head.php')?>
+<?php include('inc/nav.php')?>
+<?php require_once"./inc/dbconn.php"?>
 
 
 <div class="container">
       <div class="card card-register mx-auto mt-5">
         <div class="card-header">Register as a  Proffesional at GharSewa</div>
         <div class="card-body">
-  <form  id= "regfrom" role="form" method="POST" action="">          
+  <form    method="POST" action="">          
             <div class="form-group">
               <div class="form-label-group">
               <label for="name">NAME</label>
-                <input type="text" id="name" class="form-control" placeholder="NAME" name="name" required="required">
+                <input type="text"  class="form-control" placeholder="NAME" name="name" required="required">
                </div>
               </div>
             <div class="form-group">
               <div class="form-label-group">
               <label for="contact_number">Contact Number</label>
-                <input type="text" id="contact_number" class="form-control" placeholder="Contact Number " name="contact_number" required="required">
+                <input type="text"  class="form-control" placeholder="Contact Number " name="contact_number" required="required">
                
               </div>
             </div>
             <div class="form-group">
               <div class="form-label-group">
               <label for="occupation">Occupation</label>
-                <input type="text" id="occupation" class="form-control" placeholder="Occupation " name="occupation" required="required">
+                <input type="text"  class="form-control" placeholder="Occupation " name="occupation" required="required">
                
               </div>
             </div>
             <div class="form-group">
               <div class="form-label-group">
               <label for="address">Address</label>
-                <input type="text" id="address" class="form-control" placeholder="Address" name="address" required="required">
+                <input type="text"  class="form-control" placeholder="Address" name="address" required="required">
                
               </div>
             </div>
             <div class="form-group">
               <div class="form-label-group">
               <label for="email">Email address</label>
-                <input type="email" id="email" class="form-control" placeholder="Email address" name="email" required="required">
+                <input type="email"  class="form-control" placeholder="Email address" name="email" required="required">
                
               </div>
             </div>
             <div class="form-group">
               <div class="form-label-group">
                   <label for="password">Password</label>
-                    <input type="password" id="password" class="form-control" placeholder="Password" name="password" required="required">  
+                    <input type="password"  class="form-control" placeholder="Password" name="password" required="required">  
                 </div>
         
               </div>
               
-            <input type="submit" class="btn btn-success btn-block" name="submit"></input> 
+            <input type="submit" class="btn btn-success btn-block" name="submit" value="submit"></input> 
           </form>
           <div class="text-center">
             <a class="d-block small mt-3" href="login.php">Login Page</a>
@@ -62,11 +61,9 @@
     </div>
    </div> 
  
+   <?php 
  
- 
- 
- <?php 
- if(isset($_POST['sumbit'])){
+ if($_SERVER['REQUEST_METHOD']=='POST'){
  $name =$_POST['name'];
  $contact = $_POST['contact_number'];
  $occupation = $_POST['occupation'];
@@ -74,8 +71,20 @@
  $email = $_POST['email'];
  $pass = $_POST['password'];
  $password =md5($pass);
- $query = "INSERT INTO staff_table VALUES('$name', '$contact', '$occupation', '$address', '$email', '$password')";
- $data = mysqli_query($conn, $query);  
- }
+ $query = "INSERT INTO staff (name, contact, occupation, address, email,password) VALUES (:name, :contact, :occupation, :address, :email, :password)";
+ 
+ $stmt=$pdo->prepare($query);
+ $stmt->bindParam(':name',$name);
+ $stmt->bindParam(':contact',$contact);
+ $stmt->bindParam(':occupation',$occupation);
+ $stmt->bindParam(':address',$address);
+ $stmt->bindParam(':email',$email);
+ $stmt->bindParam(':password',$password);
 
-?>
+ $stmt->execute();  
+ 
+ }
+?> 
+ 
+ 
+ 
